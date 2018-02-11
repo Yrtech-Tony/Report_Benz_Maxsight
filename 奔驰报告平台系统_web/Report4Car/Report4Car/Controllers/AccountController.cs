07c4eb851782c371186eb75dcb09b75bf5a66b32
Tony <moujunsheng15@163.com>
2018-02-11 16:42:46 +0800

    修改了奔驰的项目介绍

--- 奔驰报告平台系统_web/Report4Car/Report4Car/Controllers/AccountController.cs
+++ 奔驰报告平台系统_web/Report4Car/Report4Car/Controllers/AccountController.cs
@@ -45,8 +45,8 @@ namespace Report4Car.Controllers
                 try
                 {
                     ReportService service = new ReportService();
-                    string role = service.GetUserRole(model.UserName, "VAN170401");
-                    Session["user"] = new User() { ID = model.UserName, Role = role, ProjectCode = "VAN170401", Year = "2017" };
+                    string role = service.GetUserRole(model.UserName, "MB1802");
+                    Session["user"] = new User() { ID = model.UserName, Role = role, ProjectCode = "MB1802", Year = "2018" };
                     FormsAuthentication.SetAuthCookie(model.UserName + "|" + role, true, "");
                     if (Request.QueryString["ReturnUrl"] != null)
                         //跳转到登录前页面
